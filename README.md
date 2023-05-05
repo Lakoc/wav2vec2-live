f9d35637bc49d82d1ca195ad4defadf058e1598a
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-05-05 22:00:22 +0200

    demo app

--- README.md
+++ README.md
@@ -20,38 +20,11 @@ Depending on linux distribution you might encounter an **error that portaudio wa
 sudo apt install portaudio19-dev
 ```
 
-Finally you can test the speech recognition:
+Download model from drive (https://drive.google.com/drive/folders/1hveY9Y3U0eBAP-qTKGzIcqzJZSilI-s-?usp=share_link) and unzip it.
+
+
+Start demo app with relative path to unzipped model.
 
 ```
-python live_asr.py
-```
-
-### Possible Issues:
-
-* The code uses the systems default audio device. Please make sure that you set your systems default audio device correctly. 
-
-* "*attempt to connect to server failed*" you can safely ignore this message from pyaudio. It just means, that pyaudio can't connect to the jack audio server. 
-
-
-## Usage
-
-You can use any **wav2vec2** model from the [huggingface model hub](https://huggingface.co/models?pipeline_tag=automatic-speech-recognition&search=wav2vec2). Just set the model name, all files will be downloaded on first execution.
-
-```python 
-from live_asr import LiveWav2Vec2
-
-english_model = "facebook/wav2vec2-large-960h-lv60-self"
-german_model = "maxidl/wav2vec2-large-xlsr-german"
-asr = LiveWav2Vec2(german_model,device_name="default")
-asr.start()
-
-try:        
-    while True:
-        text,sample_length,inference_time = asr.get_last_text()                        
-        print(f"{sample_length:.3f}s"
-        +f"\t{inference_time:.3f}s"
-        +f"\t{text}")
-        
-except KeyboardInterrupt:   
-    asr.stop()  
+python demo_app.py --model checkpoint-115000
 ```
--- demo_app.py
+++ demo_app.py
@@ -1,46 +1,57 @@
 import PySimpleGUI as sg
 from live_asr import LiveWav2Vec2
+from argparse import ArgumentParser, Namespace
 
 
-font_base = font=('Ubuntu Font Family', 10)
-# GUI layout
-layout = [
-    [sg.Button("START", key="-RECORD-", font=font_base),
-     sg.Button("PAUSE", key="-PAUSE-", disabled=True, font=font_base),
-     sg.Button("CLEAR", key="-CLEAR-", font=font_base),
-     ],
-    [sg.Text("Transcript:", font=font_base)],
-    [sg.Multiline("", size=(115, 25), font=('Ubuntu Font Familys', 20), key="-TRANSCRIPT-")],
-]
+def parse_args() -> Namespace:
+    parser = ArgumentParser(
+        description='')
+    parser.add_argument('--model', type=str, help='', required=True)
+    return parser.parse_args()
 
-# GUI window
-window = sg.Window("ASR Demo", layout,  font=font_base)
 
-asr = LiveWav2Vec2("checkpoint-115000")
+if __name__ == "__main__":
+    args = parse_args()
 
-# Event loop
-while True:
-    event, values = window.read(timeout=100)
+    font_base = font = ('Ubuntu Font Family', 10)
+    # GUI layout
+    layout = [
+        [sg.Button("START", key="-RECORD-", font=font_base),
+         sg.Button("PAUSE", key="-PAUSE-", disabled=True, font=font_base),
+         sg.Button("CLEAR", key="-CLEAR-", font=font_base),
+         ],
+        [sg.Text("Transcript:", font=font_base)],
+        [sg.Multiline("", size=(115, 25), font=('Ubuntu Font Familys', 20), key="-TRANSCRIPT-")],
+    ]
 
-    if event == sg.WIN_CLOSED:
-        break
+    # GUI window
+    window = sg.Window("ASR Demo", layout, font=font_base)
 
-    if event == "-RECORD-":
-        window["-RECORD-"].update(disabled=True)
-        window["-PAUSE-"].update(disabled=False)
-        asr.start()
+    asr = LiveWav2Vec2(args.model)
 
-    if event == "-PAUSE-":
-        window["-PAUSE-"].update(disabled=True)
-        asr.pause()
-        window["-RECORD-"].update(disabled=False)
+    # Event loop
+    while True:
+        event, values = window.read(timeout=100)
 
-    if event == "-CLEAR-":
-        window["-TRANSCRIPT-"].update("")
+        if event == sg.WIN_CLOSED:
+            break
 
-    if asr.asr_output_queue.qsize() > 0:
-        text, sample_length, inference_time, confidence = asr.get_last_text()
-        window['-TRANSCRIPT-'].update(text + '\n', append=True)
+        if event == "-RECORD-":
+            window["-RECORD-"].update(disabled=True)
+            window["-PAUSE-"].update(disabled=False)
+            asr.start()
 
-asr.stop()
-window.close()
+        if event == "-PAUSE-":
+            window["-PAUSE-"].update(disabled=True)
+            asr.pause()
+            window["-RECORD-"].update(disabled=False)
+
+        if event == "-CLEAR-":
+            window["-TRANSCRIPT-"].update("")
+
+        if asr.asr_output_queue.qsize() > 0:
+            text, sample_length, inference_time, confidence = asr.get_last_text()
+            window['-TRANSCRIPT-'].update(text + '\n', append=True)
+
+    asr.stop()
+    window.close()
